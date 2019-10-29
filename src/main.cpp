a5d039b0f9cd78e4630a4f8cd1bb9143cca7bc25
6rayWa1cher <27364356+6rayWa1cher@users.noreply.github.com>
2019-10-29 19:45:43 +0300

    added command line args

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,47 @@
 #include "include/Scanner.h"
 
 
-int main() {
+int main(int argc, char **argv) {
 	std::ifstream ifile;
-	ifile.open("test.txt");
+    std::string filename, input, output;
+    int i = 1;
+    while (i < argc) {
+        input = std::string(argv[i]);
+        if (input == "-i") {
+            if (argc <= i + 1) {
+                std::cerr << "Empty -f" << std::endl;
+                return 1;
+            }
+            filename = argv[i + 1];
+            i += 2;
+        } else if (input == "-o") {
+            if (argc <= i + 1) {
+                std::cerr << "Empty -o" << std::endl;
+                return 1;
+            }
+            output = argv[i + 1];
+            i += 2;
+        } else {
+            filename = input;
+            ++i;
+        }
+    }
+    ifile.open(filename);
 	Scanner scanner(ifile);
 	if (!ifile) {
 		std::cout << "Failed to open" << std::endl;
 	}
-	else {
-		for (;;) {
-			Token currentLexem = scanner.getNextToken();
-			currentLexem.print(std::cout);
-			if (currentLexem.type() == LexemType::error || currentLexem.type() == LexemType::eof) {
-				break;
-			}
-		}
-	}
-}
+    std::cout << "Opened input: " << filename << std::endl;
+    std::ofstream ofile;
+    ofile.open(output);
+    if (ofile) {
+        std::cout << "Opened output: " << output << std::endl;
+    }
+    Token currentLexem;
+    do {
+        currentLexem = scanner.getNextToken();
+        currentLexem.print(!ofile ? std::cout : ofile);
+    } while (!(currentLexem.type() == LexemType::error || currentLexem.type() == LexemType::eof));
+    ifile.close();
+    ofile.close();
+}
